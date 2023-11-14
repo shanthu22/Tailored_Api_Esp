4ead564936496d2c176eb1549c93a114fa1dc9d2
unknown <ssanthujan@yahoo.com>
2023-11-15 00:39:38 +0530

    db connection

--- ESP/db.js
+++ ESP/db.js
@@ -3,6 +3,16 @@ require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
-console.log(process.env, DB_HOST);
+connection.connect((err) => {
+  if (err) {
+    console.log("Error in DB connection " + err.stack);
+  }
+});
+
+module.exports = connection;
--- ESP/index.js
+++ ESP/index.js
@@ -1,3 +1,25 @@
 console.log("aaaa");
-
+require("dotenv").config();
 const express = require("express");
+console.log(process.env.DB_HOST);
+
+const mysql = require("mysql2");
+//require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Error in DB connection " + err.stack);
+  } else {
+    console.log("Connection to DB successfully");
+  }
+});
+
+module.exports = connection;
