ff3b54d39009a4b7d7d7feaddcb50570544e822b
unknown <ssanthujan@yahoo.com>
2023-11-24 14:56:46 +0530

    cors fix

--- ESP/index.js
+++ ESP/index.js
@@ -1,14 +1,26 @@
-console.log("aaaa");
+//index.js
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
-console.log(process.env.DB_HOST);
+const UXSRoutes = require("./api/userxservice/userxservice.router");
 
 const app = express();
-const UXSRoutes = require("./api/userxservice/userxservice.router");
 app.use("/esp", UXSRoutes);
+//CORS
+const allowedOrigins = [`http://localhost:${process.env.PORT}`];
+const CorsOption = {
+  origin: allowedOrigins,
+  methods: "GET,HEAD,PUT",
+  credentials: true,
+};
+app.use(cors(CorsOption));
+
+//testing end point
 app.get("/", (req, res) => {
-  res.send("it works boss ");
+  res.send("Works");
 });
+
+//listening on the given PORT
 app.listen(process.env.PORT, () => {
   console.log(`Listening on Port: http://localhost:${process.env.PORT}`);
 });
